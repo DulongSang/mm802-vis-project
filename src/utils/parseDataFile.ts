5640f149d0e374477ec664d9d21a5269f7919975
Dulong Sang <dulong@ualberta.ca>
2024-03-19 13:13:26 -0600

    refactor: replace danfojs with JS object for data storage

--- src/utils/parseDataFile.ts
+++ src/utils/parseDataFile.ts
@@ -1,15 +1,14 @@
-import { DataFrame } from 'danfojs';
 import Papa from 'papaparse';
 
 import { FireResponseDataRow, ResponseCode, DayOfWeek, EventDescription } from '../types/DataSetInfo';
 
-export async function parseDataFile(file: File): Promise<DataFrame> {
-    return new Promise<DataFrame>((resolve, reject) => {
+export async function parseDataFile(file: File): Promise<FireResponseDataRow[]> {
+    return new Promise((resolve, reject) => {
         Papa.parse(file, {
         header: true,
         skipEmptyLines: true,
         complete: (results) => {
-            resolve(new DataFrame(parseData(results.data)));
+            resolve(parseData(results.data));
         },
         error: (error: any) => {
             reject(error);
@@ -24,7 +23,7 @@ function parseData(data: any[]): FireResponseDataRow[] {
             year: parseInt(row.year),
             month: parseInt(row.month),
             dayofweek: row.dayofweek as DayOfWeek,
-            datetime: row.datetime,
+            datetime: new Date(row.datetime),
             event_duration_mins: parseInt(row.event_duration_mins),
             event_description: row.event_description as EventDescription,
             latitude: parseFloat(row.latitude),
